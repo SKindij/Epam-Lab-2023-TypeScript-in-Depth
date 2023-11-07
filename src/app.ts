5047a064d032f7b5d691cbd8b2e0b46b144e43de
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-07 17:23:49 +0200

    feat: introduce new feature to codebase
    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 // src/app.ts
-import { logFirstAvailable } from './lib/utility-functions';
+import { Category } from './enums';
+
+import { 
+  logFirstAvailable,
+  getBookTitlesByCategory, 
+  logBookTitles,
+  getBookAuthorByIndex,
+  totalNumberOfPages
+} from './lib/utility-functions';
+
+
 
 
 // функція для виведення вітання на сторінці
@@ -10,9 +20,28 @@ function showHello(divName: string, name: string) {
 
 showHello('greeting', 'TypeScript');
 
-
-
 // Task 02
 console.log('---TASK 02: Types Basics---');
 console.log('---Завдання 02.01 Базові типи---');
+
 logFirstAvailable();
+
+const javascriptBooks = getBookTitlesByCategory(Category.JavaScript);
+  console.log('JavaScript книги:');
+  logBookTitles(javascriptBooks);
+
+console.log('---Завдання 02.02 Приведення до константи---');
+
+let bookIndex = 4;
+const bookInfo = getBookAuthorByIndex(bookIndex);
+  if (bookInfo) {
+    const [title, author] = bookInfo;
+	console.log(`Результат пошуку за індексом ${bookIndex}.`);
+    console.log(`Назва книги: ${title}`);
+    console.log(`Автор: ${author}`);
+  } else {
+    console.log(`Книга за індексом ${bookIndex} не знайдена.`);
+  }
+
+// конвертуємо значення bigint у рядок (string) і виводимо в консоль
+console.log('Загальна кількість сторінок у всіх бібліотеках: ', totalNumberOfPages.toString());
