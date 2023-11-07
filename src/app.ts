9daec23c7d1b1ecd544dc07ec2336e864240dc93
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-07 12:57:50 +0200

    feat: introduce new feature to codebase
    TASK 02: Types Basics

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
-showHello('greeting', 'TypeScript');
+// src/app.ts
+import { logFirstAvailable } from './lib/utility-functions';
 
+
+// функція для виведення вітання на сторінці
 function showHello(divName: string, name: string) {
   const elt = document.getElementById(divName);
   elt!.innerText = `Hello from ${name}`;
 }
+
+showHello('greeting', 'TypeScript');
+
+
+
+// Task 02
+console.log('---TASK 02: Types Basics---');
+console.log('---Завдання 02.01 Базові типи---');
+logFirstAvailable();
