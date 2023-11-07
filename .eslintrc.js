c167681dcc491b10c5b1be75c221c708e72e2432
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-07 11:02:02 +0200

    style: enhance code comments
    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,80 @@
 module.exports = {
-    env: {
-        browser: true
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        sourceType: 'module'
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {
-        '@typescript-eslint/naming-convention': ['error', {
-            selector: 'class',
-            format: ['PascalCase'],
-        }],
-        '@typescript-eslint/indent': 'error',
-        '@typescript-eslint/member-delimiter-style': [
-            'error',
-            {
-                multiline: {
-                    delimiter: 'semi',
-                    requireLast: true
-                },
-                singleline: {
-                    delimiter: 'semi',
-                    requireLast: false
-                }
-            }
-        ],
-        '@typescript-eslint/prefer-namespace-keyword': 'error',
-        '@typescript-eslint/quotes': ['error', 'single'],
-        '@typescript-eslint/semi': ['error', 'always'],
-        '@typescript-eslint/type-annotation-spacing': 'error',
-        'brace-style': ['error', '1tbs'],
-        camelcase: 'error',
-        eqeqeq: ['error', 'smart'],
-        'id-blacklist': [
-            'error',
-            'any',
-            'Number',
-            'number',
-            'String',
-            'string',
-            'Boolean',
-            'boolean',
-            'Undefined',
-            'undefined'
-        ],
-        'id-match': 'error',
-        'no-eval': 'error',
-        'no-redeclare': 'error',
-        'no-trailing-spaces': 'error',
-        'no-underscore-dangle': 'error',
-        'no-unsafe-finally': 'error',
-        'no-var': 'error',
-        'spaced-comment': ['error', 'always', { markers: ['/'] }],
-    }
+  env: {
+    // код призначений для виконання в браузері
+    browser: true,
+  },
+  // для аналізу коду використовується парсер TypeScript
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+	// код використовує модульний синтаксис
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+	// для заборони імен класів, які не є "PascalCase"
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'class',
+        format: ['PascalCase'],
+      },
+    ],
+	// вимагає відступи у 2 пробіли
+    '@typescript-eslint/indent': ['error', 2],
+	// встановлює стиль для роздільників полів об'єкта та типів
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      },
+    ],
+	// рекомендує використовувати ключове слово "namespace"
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+	// встановлює стиль використання одинарних лапок для рядків
+    '@typescript-eslint/quotes': ['error', 'single'],
+	// вимагає завжди ставити крапку з комою
+    '@typescript-eslint/semi': ['error', 'always'],
+	// встановлює стиль розставлення пропусків в анотаціях типів
+    '@typescript-eslint/type-annotation-spacing': ['error', { before: 'never', after: 'never' }],
+    // стиль фігурних дужок для блоків коду
+	// тут "One True Brace Style" - { в наступному рядку
+	'brace-style': ['error', '1tbs'],
+	// вимагає стиль camelCase для ідентифікаторів
+    camelcase: 'error',
+	// вимагає використання строгої рівності (===) 
+    eqeqeq: ['error', 'smart'],
+	// вказує список заборонених ідентифікаторів.
+    'id-blacklist': [
+      'error',
+      'any',
+      'Number',
+      'number',
+      'String',
+      'string',
+      'Boolean',
+      'boolean',
+      'Undefined',
+      'undefined',
+    ],
+    'id-match': 'error',
+    'no-eval': 'error',
+	// забороняє повторне оголошення змінних
+    'no-redeclare': 'error',
+	// забороняє пропуски в кінці рядків
+    'no-trailing-spaces': 'error',
+	// забороняє використання підкреслення в іменах змінних
+    'no-underscore-dangle': 'error',
+	// забороняє використання небезпечного блоку finally
+    'no-unsafe-finally': 'error',
+    'no-var': 'error',
+	// вимагає пропуски після символу коментаря
+    'spaced-comment': ['error', 'always', { markers: ['/'] }],
+  },
 };
