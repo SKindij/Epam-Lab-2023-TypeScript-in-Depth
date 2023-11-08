39c7e5f61763379c5d3708a5c63bbf0c23c17f4f
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-08 22:53:02 +0200

    feat: introduce new feature to codebase
    update interfaces.ts

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,17 +1,44 @@
 // src/interfaces.ts
 import { Category } from './enums';
 
+// оголошення інтерфейсу для книжки
 interface Book {
   id:number;
   title:string;
   author:string;
   available:boolean;
   category:Category;
-  year?: number;
-  copies?: number,
-  pages?: number
+  year?:number;
+  copies?:number;
+  pages?:number;
+  markDamaged?:DamageLogger;
+  isbn?:string;
+};
+
+// оголошення інтерфейсу для функції
+interface DamageLogger {
+  (reason:string):void;
+};
+
+// інтерфейси для різних користувачів
+interface Person {
+  name:string;
+  email:string;
+};
+interface Author extends Person {
+  numBooksPublished:number;
+};
+interface Librarian extends Person {
+  department:string;
+  assistCustomer(custName:string, bookTitle:string):void;
 };
 
 
 
-export { Book };
+
+
+
+
+
+
+export { Book, DamageLogger, Author, Librarian, Person };
