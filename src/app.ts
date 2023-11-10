b8cfd71db5d8d6841054f38f05adc0873807f807
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-10 23:38:57 +0200

    refactor: reorganize existing codebase
    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,12 @@
 // src/app.ts
 import { Category } from './enums';
-import { Book, DamageLogger, Author, Librarian  } from './interfaces';
-import { ReferenceItem, Encyclopedia } from './classes';
+import {  } from './interfaces';
+import { RefBook, UL, ReferenceItem } from './classes';
+import { Library } from './classes/library';
 
 import {
-  logFirstAvailable,
-  getBookTitlesByCategory,
-  logBookTitles,
-  getBookAuthorByIndex,
-  totalNumberOfPages,
-  createCustomerID,
-  createCustomer,
-  getBookByID,
-  checkoutBooks,
-  getTitles,
-  bookTitleTransorm,
-  printBook,
-  getProperty
+  printRefBook
 } from './lib/functions';
-
-const favoriteAuthor:Author = {
-  name: 'John Doe',
-  email: 'johndoe@example.com',
-  numBooksPublished: 10,
-};
-
-const favoriteLibrarian:Librarian = {
-  name: 'Jane Smith',
-  email: 'janesmith@example.com',
-  department: 'Fiction',
-  assistCustomer: (custName, bookTitle) => {
-    console.log(`Assisting customer ${custName} with the book "${bookTitle}"`);
-  },
-};
 
 // функція для виведення вітання на сторінці
 function showHello(divName:string, name:string) {
@@ -42,33 +16,15 @@ function showHello(divName:string, name:string) {
 
 showHello('greeting', 'TypeScript');
 
+// Task 07
+console.log('--- Generics ---');
+console.log('--- Завдання 07.01. Загальні функції ---');
 
-// Task 05 Classes
-console.log('--- TASK 05: Classes ---');
-console.log('--- Завдання 05.01. Створення та використання класів ---');
 
-logFirstAvailable();
 
-console.log('--- Завдання 05.02. Розширення класів ---');
 
-// створення нового об'єкта Encyclopedia
-const refBook = new Encyclopedia(11, 'Encyclopedia of Science', 2023, 2);
 
-// ініціалізуємо властивість _publisher та виведемо її в консоль
-refBook.publisher = 'example publisher';
-console.log(refBook.publisher); // => EXAMPLE PUBLISHER
 
-// виводимо об'єкт у консоль
-console.dir(refBook); // output => Encyclopedia{...}
 
-// викликаємо різні методи
-console.log('--- Properties of refBook ---');
-console.log(`ID: ${refBook.getID()}`); // output => ID: 11 
-refBook.printItem(); 
-/* output:
- => Encyclopedia of Science was published in 2023
- => Department: Default Department
- => Edition: 2 (2023)
-*/
-refBook.printCitation(); // output => Encyclopedia of Science - 2023
+
 
