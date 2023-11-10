62515daa0da4c646e38fe719c4c6711f4edab84e
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-10 23:24:26 +0200

    init: add an additional file
    create reader.ts

---
+++ src/classes/reader.ts
@@ -0,0 +1,11 @@
+// src/classes/reader.ts
+import { Book } from '../interfaces';
+
+export class Reader {
+  name:string = '';
+  books:Book[] = [];
+
+  takeBook(book:Book):void {
+    this.books.push(book);
+  }
+}
