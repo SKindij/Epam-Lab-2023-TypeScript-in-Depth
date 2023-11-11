22559bfbf3362cb9387280693e91a97c5469c85b
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-11 21:07:43 +0200

    feat: incorporate new user-facing feature
    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ showHello('greeting', 'TypeScript');
 
 // *імпорти з інших модулей
 import { Category } from './enums';
-import { Book } from './interfaces';
-import { } from './classes';
+import { Book, Magazine } from './interfaces';
+import { Shelf } from './classes';
 import { } from './classes/library';
 
 import {
@@ -26,15 +26,29 @@ const inventory:Book[] = [
   { id: 12, title: '8-Bit Graphics with Cobol', author: 'A. B.', available: true, category: Category.Software },
   { id: 13, title: 'Cool autoexec.bat Scripts!', author: 'C. D.', available: true, category: Category.Software }
 ];
-console.log(purge(inventory));
-console.log(purge([101, 102, 103, 104, 105, 106, 107]));
+// console.log(purge(inventory));
+// console.log(purge([101, 102, 103, 104, 105, 106, 107]));
 
 // let purgeNumbers = purge<number>(inventory:number[]);
 
 console.log('--- Завдання 07.02. Загальні інтерфейси і класи ---');
+// створюємо екземпляр класу Shelf
+const bookShelf = new Shelf<Book>();
+// зберігаємо усі книжки з inventory в bookShelf
+inventory.forEach(book => bookShelf.add(book));
+// виводимо назву першої книжки
+console.log(bookShelf.getFirst().title);
 
-
-
+const magazines:Magazine[] = [
+  { title: 'Programming Language Monthly', publisher: 'Code Mags' },
+  { title: 'Literary Fiction Quarterly', publisher: 'College Press' },
+  { title: 'Five Points', publisher: 'GSU' }
+];
+// створюємо екземпляр класу Shelf
+const magazineShelf = new Shelf<Magazine>();
+// зберігаємо усі дурнали з magazines в magazineShelf
+magazines.forEach(mag => magazineShelf.add(mag));
+console.log(magazineShelf.getFirst().title);
 
 
 
