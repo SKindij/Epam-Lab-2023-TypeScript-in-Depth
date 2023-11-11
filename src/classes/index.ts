4eec8b595dd58ac803d1c043ee2f331ec5788057
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-11 20:45:47 +0200

    chore: perform general project maintenance tasks
    update index.ts

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -4,3 +4,4 @@ export { default as RefBook } from './encyclopedia';
 export * as UL from './university-librarian';
 export * from './reader';
 export type { Library } from './library';
+export { default as Shelf } from './shelf';
