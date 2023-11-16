50857ce9e5da31127ab892ffee998a9187a7732c
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-16 15:41:57 +0200

    chore: refactor code to maintain code style consistency
    update index.ts

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,7 +1,20 @@
 // src/classes/index.ts
+// у TS існують різні способи експорту модулів:
+
+// отримання всіх іменованих експортів з файлу
 export * from './reference-item';
+
+// дозволяє перейменувати дефолтний експорт на призначене ім'я
 export { default as RefBook } from './encyclopedia';
+
+// всі іменовані експорти доступні через простір імен UL
 export * as UL from './university-librarian';
+
+// тут експортується все із файлу 
 export * from './reader';
+
+// тут експортується тільки конкретний тип 
 export type { Library } from './library';
+
+// дефолтний експорт з файлу 
 export { default as Shelf } from './shelf';
