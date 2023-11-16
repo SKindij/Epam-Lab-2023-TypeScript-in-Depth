af0147835f94890f7d2d1506f837357f9dabf39b
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-16 13:56:30 +0200

    chore: refactor code to maintain code style consistency
    update shelf.ts

--- src/classes/shelf.ts
+++ src/classes/shelf.ts
@@ -1,6 +1,7 @@
 // path: src/classes/shelf.ts
 import { ShelfItem } from '../interfaces';
 
+
 export default class Shelf<T extends ShelfItem> {
   private items:T[];
 
@@ -9,15 +10,19 @@ export default class Shelf<T extends ShelfItem> {
     this.items = [];
   }
   // додаємо нову книжку на поличку
-  add(item:T):void {
-    this.items.push(item);
+  add(item:T): void {
+    if (typeof item === 'object') {
+      this.items.push(item);
+    } else {
+      console.error('Invalid item type. Only objects can be added to the shelf.');
+    }
   }
   // отримуємо першу найближчу книжку
   getFirst():T {
     return this.items[0];
   }
   // повертає першу знайдену книжку
-  find(title:string):T {
+  find(title:string):T|undefined {
     return this.items.find((item:T) => item.title === title);
   }
   // виводить назви книжок, що є на поличці
