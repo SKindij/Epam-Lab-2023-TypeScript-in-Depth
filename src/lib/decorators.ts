6fa1b0d5da1ee8d25d2f162259dbedb3aff62d0f
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-17 14:55:09 +0200

    feat: introduce new feature to codebase
    update decorators.ts

--- src/lib/decorators.ts
+++ src/lib/decorators.ts
@@ -1,20 +1,49 @@
 // src/lib/decorators.ts
 
 // декоратор для заморожування як самого конструктора, так і його прототипу
-export function freeze(target:Function, param:string): void {
-  // "target" - це сам конструктор класу (функція-конструктор),
-  // "param" - рядок, який буде виведений у консоль для інформаційних цілей
-  console.log(`Freezing the constructor ${param}`);
-  // вже існуючі властивості конструктора не можна буде змінювати
-  Object.freeze(target);
-  // заморожує прототип об'єкта, який створює екземпляри класу
-  Object.freeze(target.prototype);
+export function freeze(param:string) {
+   // "param" - рядок виведення у консоль для інформаційних цілей
+  return function <T extends { new (...args:any[]):{} }>(constructor:T): void {   
+    console.log(`Freezing the constructor ${param}`);
+    // вже існуючі властивості конструктора не можна буде змінювати
+    Object.freeze(constructor);
+    // заморожує прототип об'єкта, який створює екземпляри класу
+    Object.freeze(constructor.prototype);
+  };
 };
 
 
-
-
-
+export function logger<TFunction extends new (...args:any[]) => any>(
+  originalClass:TFunction
+):TFunction {
+  // створює нову функцію-конструктор
+  const newConstructor:Function = function(...args:any[]) {
+    console.log('Creating new instance');
+    console.log(originalClass.name);
+	// викликає оригінальний конструктор та передає йому всі аргументи
+	const instance = new originalClass(...args);
+    Object.assign(this, instance);
+    this.age = 30;
+  };
+  // встановлює прототип нового конструктора таким же, як у вихідному конструкторі
+  // дозволяє новому конструктору успадковувати методи та властивості вихідного конструктора
+  newConstructor.prototype = Object.create(originalClass.prototype);
+  // Object.setPrototypeOf(newConstructor.prototype, constructor.prototype);
+  // додає новий метод до прототипу нового конструктора
+  newConstructor.prototype.printLibrarian = function ():void {
+    console.log(`Librarian name: ${this.name}, Librarian age: ${this.age}`);
+  };
+  // повертає новий конструктор
+  return newConstructor as TFunction;
+}
+/* 
+  "extends new (...args:any[]) => any" - вказує, що TFunction повинен бути конструктором, 
+  тобто функцією, яку можна викликати за допомогою new, 
+  і вона повинна приймати будь-яку кількість аргументів;
+  
+  "function(...args: any[]) { ... }" - визначаємо тіло функції-конструктора, 
+  яке буде викликане при створенні нового екземпляра класу;
+*/
 
 
 
