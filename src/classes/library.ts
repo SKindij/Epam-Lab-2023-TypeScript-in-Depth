0acc4529b070328cf506d64d1c301dbb20541f4c
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-17 16:38:22 +0200

    chore: refactor code to maintain code style consistency
    update library.ts

--- src/classes/library.ts
+++ src/classes/library.ts
@@ -1,6 +1,10 @@
 // src/classes/library.ts
+import { setInitial } from '../lib/decorators';
+
 export class Library {
+  @setInitial(81)
   id:number | undefined;
+  
   name:string | undefined;
   address:string | undefined;
 }
