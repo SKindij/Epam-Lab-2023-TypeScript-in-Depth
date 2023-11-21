357f64ea98d0d1e2a4e48910caa8fc2a23a570fb
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-21 12:12:03 +0200

    feat: introduce new feature to codebase
    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,78 +7,23 @@ function showHello(divName:string, name:string) {
 showHello('greeting', 'TypeScript');
 
 // *імпорти з інших модулей
-// import {  } from './enums';
+import { Category } from './enums';
 // import {  } from './interfaces';
 // import {  } from './types';
-// import {  } from './lib/functions';
-import { UniversityLibrarian } from './classes/university-librarian';
-import { Library } from './classes/library';
-import { RefBook } from './classes';
+// import {  } from './classes';
+import { getBooksByCategory, logCategorySearch } from './lib/functions';
 
-// *TODO: Task 08
-console.log('--- Decorators ---');
-console.log('--- Завдання 08.01. Декоратор класу ---');
 
-// створення екземпляра класу UniversityLibrarian
-const librarian = new UniversityLibrarian('John Doe', 'john.doe@example.com', 'Library Science');
-// демонструємо виклик методу класу
-librarian.assistCustomer('Alice', 'Introduction to TypeScript');
+// *TODO: Task 09
+console.log('--- Asynchronous Patterns ---');
+console.log('--- Завдання 09.01. Функція зворотнього виклику ---');
 
-// спроба додати нову властивість після заморожування
-// librarian.newProperty = 'This should cause an error';
-// => Property 'newProperty' does not exist on type 'UniversityLibrarian'.
+console.log('Before calling getBooksByCategory');
+getBooksByCategory(Category.JavaScript, logCategorySearch);
+getBooksByCategory(Category.Software, logCategorySearch);
+console.log('After calling getBooksByCategory');
 
-console.log('--- Завдання 08.02. Декоратор класу ---');
 
-const fLibrarian = new UniversityLibrarian('Anna', 'anna@email.com', 'Library Science');
-// виведення інформації у консоль
-console.log(`Librarian: ${fLibrarian.name}, Email: ${fLibrarian.email}, Department: ${fLibrarian.department}`);
-
-console.log('--- Завдання 08.03. Декоратор методу ---');
-
-fLibrarian.assistFaculty();
-fLibrarian.teachCommunity();
-/* output
-  => university-librarian.ts:29 Anna Assisting faculty
-  => Anna Teaching community
-*/
-
-console.log('--- Завдання 08.04. Декоратор методу ---');
-
-const encyclopedia48 = new RefBook(48, 'Encyclopedia of TypeScript', 2023, 4);
-encyclopedia48.printItem();
-
-console.log('--- Завдання 08.05. Декоратор поля ---');
-
-const library = new Library();
-// виводимо значення id у консоль
-console.log(`Library: id ${library.id}`);
-
-console.log('--- Завдання 08.06. Декоратор автоаксесора ---');
-
-// створюємо екземпляр класу UniversityLibrarian
-const librarianInstance = new UniversityLibrarian();
-// встановлюємо значення для властивості name
-librarianInstance.name = 'John Doe';
-// отримуємо та виводимо значення властивості name у консоль
-console.log(librarianInstance.name); // => Mr./Mrs. John Doe
-
-console.log('--- Завдання 08.07. Декоратор аксесорів ---');
-
-// створення екземпляра класу Encyclopedia
-const encyclopedia71 = new RefBook(71, 'Encyclopedia of Epam', 1999, 2);
-
-// спроба встановлення різних значень
-try {
-  encyclopedia71.copies = 15;
-
-  encyclopedia71.copies = -4; 
-} catch (error:any) {
-   // виведе: "Invalid value for copies. Must be a positive integer."
-  console.error(error.message);
-};
-
-console.log("RefBook71 copies:", encyclopedia71.copies);
 
 
 
